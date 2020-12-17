6de929439c0dd14bc3229923e716be90e7c17ef9
Shima <46078198+Shima-710@users.noreply.github.com>
2020-12-17 18:45:50 +0900

    v3.0

---
+++ plugin/src/main/java/com/github/shima710/redapple/RedApple.java
@@ -0,0 +1,17 @@
+package com.github.shima710.redapple;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RedApple extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
